9542b45f05f9baf2acedde70e3e2cece1d000f8b
Jacquin Wynn <jacquinwynn@Jacquins-MacBook-Pro.local>
2016-07-13 14:07:29 -0400

    Added Signup and Login Functionality

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var authenticationController = require('./server/controllers/authentication-controller');
 
 var app = express();
 
-mongoose.connect('mongod://localhost:27017/socialnetwork');
+mongoose.connect("mongodb://localhost:27017/socialnetwork");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/app', express.static(__dirname + "/app"));
+app.use('/node_modules', express.static(__dirname + "/node_modules"));
 
 
 app.get('/', function(req, res){
-   res.sendFile('index');
+   res.sendfile('index.html');
 });
 
-
-
+//Authentication
+app.post('/api/user/signup', authenticationController.signup);
+app.post('/api/user/login', authenticationController.login);
 
 
 
