ae14c3cf8cc6fa94816a0ddbe66e35bbe56d4236
Jacquin Wynn <jacquinwynn@Jacquins-MacBook-Pro.local>
2016-07-13 22:54:48 -0400

    Added image upload, edit username and add bio functionality

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var authenticationController = require('./server/controllers/authentication-controller');
+var profileController = require('./server/controllers/profile-controller');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
 
 var app = express();
 
@@ -9,6 +12,7 @@ mongoose.connect("mongodb://localhost:27017/socialnetwork");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multipartMiddleware);
 app.use('/app', express.static(__dirname + "/app"));
 app.use('/node_modules', express.static(__dirname + "/node_modules"));
 
@@ -21,8 +25,10 @@ app.get('/', function(req, res){
 app.post('/api/user/signup', authenticationController.signup);
 app.post('/api/user/login', authenticationController.login);
 
-
-
+//Profile
+app.post('/api/profile/editPhoto', multipartMiddleware, profileController.updatePhoto);
+app.post('/api/profile/updateUsername', profileController.updateUsername);
+app.post('/api/profile/updateBio', profileController.updateBio);
 
 
 
